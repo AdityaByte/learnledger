cf0da67453e60ea89450b22c7610baf841f5372e
AdityaByte <adityapawar4914@gmail.com>
2024-09-24 23:27:26 +0530

    in the js file it sends data of form using jquery or ajax asynchronously and display the response of success and error at console after so much headache i figure out the 404 issue in that

--- src/main/webapp/WEB-INF/resources/js/organization.js
+++ src/main/webapp/WEB-INF/resources/js/organization.js
@@ -1,17 +1,45 @@
-document.getElementById('orgForm').addEventListener('submit' , function(event){
-    event.preventDefault();
-    let formData = new FormData(this);
-    $.ajax({
-        url: "/org/form",
-        type: "POST",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function (response) {
-            console.log("sucess - " , response);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error("error" , errorThrown);
-        }
+const backButton = document.getElementById('backButton');
+
+if (backButton) {
+    backButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        window.history.back();
+    });
+}
+
+const orgForm = document.getElementById('orgForm');
+
+document.addEventListener("DOMContentLoaded" , function(){
+    orgForm.addEventListener('submit' , (event) => {
+        event.preventDefault();
+        let formData = new FormData(orgForm);
+        $.ajax({
+            url: 'form',
+            method: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response){
+                console.log("Full response : " , response);
+                console.log("Reponse Status : ",response.status);
+            },
+            error: function(error){
+                console.log("Error : " , error);
+            }  
+        });
     });
 });
+
+function displayFileName1(event) {
+    const input = event.target;
+    const fileName = input.files[0] ? input.files[0].name : "No file selected";
+    
+    document.getElementById("file-name1").textContent = fileName;
+}
+
+function displayFileName2(event) {
+    const input = event.target;
+    const fileName = input.files[0] ? input.files[0].name : "No file selected";
+    
+    document.getElementById("file-name2").textContent = fileName;
+}
