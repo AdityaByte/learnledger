7ab52d3a75b624072f66c5b12cb6d7f1604a79fb
AdityaByte <adityapawar4914@gmail.com>
2024-10-02 16:22:51 +0530

    Done many changes and fix many bugs and added the content section for displaying the content

--- src/main/java/com/learnledger/controller/HomeController.java
+++ src/main/java/com/learnledger/controller/HomeController.java
@@ -6,7 +6,6 @@ import com.learnledger.service.CardService;
 import com.learnledger.service.DataService;
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,11 +14,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+
+    public HomeController() {
+        System.out.println("i am the controller being called by someone @");
+    }
+    
 
     @Autowired
     private DataService service;
@@ -67,7 +72,7 @@ public class HomeController {
                         "pdfs", new ArrayList<>(),
                         "docs", new ArrayList<>()
                     )
-                );    
+                );
             }
         }
         else{
@@ -86,5 +91,31 @@ public class HomeController {
         return "home";
     }
 
-    
+    @GetMapping("/content/{contentNo}")
+    public String showContent(@PathVariable("contentNo") int contentNo , Model model){
+        List<DataDocument> notes = service.findByFieldName(DocumentType.NOTES);
+        List<DataDocument> projects = service.findByFieldName(DocumentType.PROJECTS);
+        List<DataDocument> pdfs = service.findByFieldName(DocumentType.PDFS);
+        List<DataDocument> docs = service.findByFieldName(DocumentType.DOCS);
+
+        switch(contentNo){
+            case 1:
+                model.addAttribute("data" , notes);
+                break;
+            case 2:
+                model.addAttribute("data" , projects);
+                break;
+            case 3:
+                model.addAttribute("data" , pdfs);
+                break;
+            case 4:
+                model.addAttribute("data" , docs);
+                break;
+            default:
+                System.out.println("Invalid url routing");
+                break;
+        }
+        
+        return "content";
+    }
 }
--- src/main/webapp/WEB-INF/resources/js/home.js
+++ src/main/webapp/WEB-INF/resources/js/home.js
@@ -6,3 +6,7 @@ function logIn(){
 function accountDetail(){
     window.location.href = "http://localhost:9494/learnledger/account";
 }
+
+function content(contentNo){
+    window.location.href = "http://localhost:9494/learnledger/home/content/" + contentNo;
+}
