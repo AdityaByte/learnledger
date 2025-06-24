ad693a1a4f48effaf01c2d780c8df435911e64e6
Aditya-Byte <adityabyte@gmail.com>
2025-06-24 16:24:13 +0530

    chore: added health route

---
+++ src/main/java/com/learnledger/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.learnledger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String healthHandler() {
+        return "Health is ok";
+    }
+
+}
